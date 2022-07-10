b5ab4f245cc3441d636cb0857113ff9dd1eed25f
ngimdock <ngimdockzemfack@gmail.com>
2022-07-10 16:28:33 +0100

    Implementation des Generics et allias

--- src/app.ts
+++ src/app.ts
@@ -4,11 +4,23 @@ console.log("Implementation des allias et Génerics");
 type User = { name: string; age: number };
 type DateScring = string;
 type Id = number | string;
+type UserName = User["name"];
+type OtherUser = keyof User;
 
 //utilisation des allias de types
 const person: User = { name: "ngimdock", age: 22 };
 const birthDay: DateScring = "09-06-2000";
 
-function deleteUser(idUser: Id): void {
-  console.log(idUser);
+function identity<ArgType>(arg: ArgType): ArgType {
+  return arg;
 }
+
+const val = identity(10);
+
+function firtTableValue<TabType>(tab: TabType[]): TabType {
+  return tab[0];
+}
+
+const value = firtTableValue(["hello", "world", "i am dan"]);
+
+const compteur = document.querySelector<HTMLButtonElement>("#increment");
