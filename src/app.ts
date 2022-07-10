7be7213eba65f156711e33668ae32d820c3dcc49
ngimdock <ngimdockzemfack@gmail.com>
2022-07-10 15:20:40 +0100

    Implementation du Narrowing ok!

--- src/app.ts
+++ src/app.ts
@@ -1,51 +1,75 @@
-// const a: string = "hello world";
+console.log("APPRENDRE LE NARROWING SUR TYPESCRIPT");
 
-// const b: number = 10;
+// Test des types primitifs
+function printId(id: string | number) {
+  if (typeof id === "number") console.log(id * 10);
+  else console.log(id.toUpperCase());
+}
 
-// const c: boolean = false;
+// Test des types primitifs
+function testVariableDeTypePrimaire(a: number | string, b: boolean | string) {
+  if (a === b) {
+    console.log(
+      " Dans ce scope d'execution, typescript saura que a et b sont de type string car c'est le seul type qu'ils ont en cummun il faut survoler les deux variables pour voir leur types"
+    );
 
-// const d: null = null;
+    console.log({ valeurA: a.toUpperCase(), valeurB: b.toUpperCase() });
+  } else {
+    console.log(
+      "dans ce scop d'exécution typescript saura que a et b sont de typ diffiffent. Survoler les variables a et b pour voir leur types"
+    );
 
-// const arr: string[] = ["1", "2", "salut"];
+    console.log({ a, b });
+  }
+}
 
-// const sayHllo = { hello: "hi!", no: "no hello" };
+// Test des types conplexes
+function testVariableDeTypeComplexe(a: string | Date) {
+  if (a instanceof Date) {
+    console.log(
+      " Typescript sait que dans ce scope, a est un objet de type Date. il faut survoler a pour voir sont type "
+    );
 
-// const user: {
-//   name: string;
-//   userName?: string;
-//   age: number;
-// } = {
-//   name: "ngimdock",
-//   age: 22,
-// };
+    console.log(a.getDay);
+  } else {
+    console.log(
+      " Typescript sait que dans ce scope, a est de type string. il faut survoler a pour voir sont type "
+    );
 
-// const abj: { [key: string]: string } = { name: "danilix" };
+    console.log(a.toUpperCase());
+  }
+}
 
-// const date: Date = new Date();
+//Test des tableaux
+function testVariableDeTypeTableau(a: number[] | string) {
+  if (Array.isArray(a)) {
+    console.log(a[0]);
+  } else {
+    console.log(a.toString);
+  }
+}
 
-// const increment = document.querySelector("#increment") as HTMLButtonElement;
+// Tester avec l'option "in"
 
-// const callBack: (event: MouseEvent) => void = (e: MouseEvent): number => {
-//   return 3;
-// };
+function exemple(a: MouseEvent | HTMLInputElement) {
+  if ("value" in a) {
+    console.log(
+      " Dans ce scope typescript saura que a est de type HTMLInputElement car cet object à une clé 'value' "
+    );
 
-// function printId(id: number | string): void {
-//   console.log(id.toString());
-// }
+    console.log(a.value);
+  } else {
+    console.log(a.clientX);
+  }
+}
 
-console.log("Get started with typescript");
+// customser ses types
+function isDate(a: any): a is Date {
+  return a instanceof Date;
+}
 
-const increment = document.querySelector("button");
-const num = document.querySelector("span");
-
-let i = 0;
-
-const incrementNum = (e: Event) => {
-  e.preventDefault();
-
-  i++;
-
-  if (num) num.innerHTML = i.toString();
-};
-
-increment?.addEventListener("click", incrementNum);
+function exempleTest(b: string | Date): void {
+  if (isDate(b)) {
+    console.log(b);
+  }
+}
