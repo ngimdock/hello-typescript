07e8f6f2ae59758729f481d93660e138a14b20d0
ngimdock <ngimdockzemfack@gmail.com>
2022-07-10 14:38:33 +0100

    Reformatage de code typescript après avoir activé le mode strict

--- src/app.ts
+++ src/app.ts
@@ -1,46 +1,51 @@
-const a: string = "hello world";
+// const a: string = "hello world";
 
-const b: number = 10;
+// const b: number = 10;
 
-const c: boolean = false;
+// const c: boolean = false;
 
-const d: null;
+// const d: null = null;
 
-const arr: string[] = ["1", "2", "salut"];
+// const arr: string[] = ["1", "2", "salut"];
 
-const user: {
-  name: string;
-  userName?: string;
-  age: number;
-} = {
-  name: "ngimdock",
-  age: 22,
-};
+// const sayHllo = { hello: "hi!", no: "no hello" };
 
-const abj: { [key: string]: string } = { name: "danilix" };
-
-const date: Date = new Date();
-
-const increment = document.querySelector("#increment") as HTMLButtonElement;
-
-const callBack: (event: MouseEvent) => void = (e: MouseEvent): number => {
-  return 3;
-};
-
-function printId(id: number | string): void {
-  console.log(id.toString());
-}
-
-// console.log("Get started with typescript");
-
-// const increment = document.querySelector("#increment");
-// const num = document.querySelector("#num");
-
-// let i = 0;
-
-// const incrementNum = (e) => {
-//   i++;
-//   num.innerHTML = i.toString();
+// const user: {
+//   name: string;
+//   userName?: string;
+//   age: number;
+// } = {
+//   name: "ngimdock",
+//   age: 22,
 // };
 
-// increment.addEventListener("click", incrementNum);
+// const abj: { [key: string]: string } = { name: "danilix" };
+
+// const date: Date = new Date();
+
+// const increment = document.querySelector("#increment") as HTMLButtonElement;
+
+// const callBack: (event: MouseEvent) => void = (e: MouseEvent): number => {
+//   return 3;
+// };
+
+// function printId(id: number | string): void {
+//   console.log(id.toString());
+// }
+
+console.log("Get started with typescript");
+
+const increment = document.querySelector("button");
+const num = document.querySelector("span");
+
+let i = 0;
+
+const incrementNum = (e: Event) => {
+  e.preventDefault();
+
+  i++;
+
+  if (num) num.innerHTML = i.toString();
+};
+
+increment?.addEventListener("click", incrementNum);
