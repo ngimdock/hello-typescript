e641dadb4c0af233b9ca1b17d6ac3b68aa10c323
ngimdock <ngimdockzemfack@gmail.com>
2022-07-16 08:12:04 +0100

    Enumérateurs ok git add .

--- src/app.ts
+++ src/app.ts
@@ -1,29 +1,25 @@
-// Le type unknown est presque même que le type any, la différence est
-// que le unknown on vas faire la vérification de type avant de faire les actions or avec le any pas besion de
-// faire des vérifications
-function verifType(arg: unknown): void {
-  if (arg instanceof HTMLInputElement) {
-    arg.value = "helo world !";
-  }
+// On peux utiliser les enum quand on à besoin de sauvegarder des états donnés
+
+// Par défaut typescript attribut une clé à chaque élément qui est son indice
+enum STEPS {
+  intro,
+  selection,
+  panier,
+  paiement,
 }
 
-// Les tuples
-const a = { isPavate: true, localhost: 3000 } as const;
-const b = [1, 2, 11] as const;
-
-const tuple: [string, number] = ["hello", 20]; //tuple non mutable et modifiable
-const tab: (string | number)[] = [12, 21, 77, "world !"]; // tableau dynamique mutable et modifiabe
-
-type ListItem = [string, number];
-
-const c: ListItem = ["yo!", 1];
-const d: ListItem = ["eh oh!", 2];
-
-function merge<T extends unknown[], U extends unknown[]>(
-  a: T,
-  b: U
-): [...T, ...U] {
-  return [...a, ...b];
+// on peux nous même définir les clé par des chaines de caractères par exemple
+enum STEPS2 {
+  intro = "intro",
+  selection = "selection",
+  panier = "panier",
+  paiement = "paiement",
 }
 
-const e = merge(c, [1, 2, 3]);
+const step: STEPS = STEPS.selection;
+const step2: STEPS2 = STEPS2.paiement;
+
+console.log(step);
+console.log(STEPS[step]);
+
+console.log(step2);
