12bee6dc5513164f57de267304e779e9297ad799
ngimdock <ngimdockzemfack@gmail.com>
2022-07-18 14:30:06 +0100

    dernier commit

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,17 @@
-export class Point {
-  x = 0;
-  y = 0;
+type User = { name: string; age: number; contry: string };
+type DateString = string;
+type StringNumber = string | number;
 
-  move(x: number, y: number) {
-    this.x += x;
-    this.y += y;
+const user1: User = { name: "Ngimdock", age: 22, contry: "cameroun" };
+const date: DateString = "09/06/2000";
+const id: StringNumber = 12344475521;
 
-    return this;
-  }
+console.log({ user1, date, id });
+
+function identity<ArgType>(arg: ArgType): ArgType {
+  return arg;
 }
 
-ga("event", {
-  hitType: "localhost",
-  eventCategory: "humble",
-});
+const person = identity<User>(user1);
+
+console.log(person.name);
